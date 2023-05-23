438a6f5cb18df85c025f42104e08463f4233f62d
DaveVodrazka <dave.vodrazka@gmail.com>
2023-05-23 16:13:11 +0200

    fix: add more debug logs

--- carmine-api-cache/src/lib.rs
+++ carmine-api-cache/src/lib.rs
@@ -55,14 +55,28 @@ impl Cache {
     }
 
     pub fn get_app_data(&self) -> AppData {
+        let all_non_expired = self.get_all_non_expired();
+        println!("Got non expired");
+        let trade_history = self.get_trade_history();
+        println!("Got trade history");
+        let option_volatility = get_options_volatility(&self.network);
+        println!("Got volatility");
+        let state_eth_usdc_call = get_pool_state(self.call_pool_address, &self.network);
+        println!("Got call pool state");
+        let state_eth_usdc_put = get_pool_state(self.put_pool_address, &self.network);
+        println!("Got put pool state");
+        let apy_eth_usdc_call = self.calculate_apy_for_pool(&self.call_pool_address);
+        println!("Got call APY");
+        let apy_eth_usdc_put = self.calculate_apy_for_pool(&self.put_pool_address);
+        println!("Got put APY");
         AppData {
-            all_non_expired: self.get_all_non_expired(),
-            trade_history: self.get_trade_history(),
-            option_volatility: get_options_volatility(&self.network),
-            state_eth_usdc_call: get_pool_state(self.call_pool_address, &self.network),
-            state_eth_usdc_put: get_pool_state(self.put_pool_address, &self.network),
-            apy_eth_usdc_call: self.calculate_apy_for_pool(&self.call_pool_address),
-            apy_eth_usdc_put: self.calculate_apy_for_pool(&self.put_pool_address),
+            all_non_expired,
+            trade_history,
+            option_volatility,
+            state_eth_usdc_call,
+            state_eth_usdc_put,
+            apy_eth_usdc_call,
+            apy_eth_usdc_put,
         }
     }
 
