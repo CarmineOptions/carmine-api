d52f7987d48072d3fe17400f8b969ee2dc60e8cd
DaveVodrazka <dave.vodrazka@gmail.com>
2023-06-20 13:28:33 +0200

    fix: remove unused imports

--- carmine-api-starknet/src/bin/fetch.rs
+++ carmine-api-starknet/src/bin/fetch.rs
@@ -1,8 +1,7 @@
 use std::env;
 
-use carmine_api_core::network::{Network, Protocol};
-use carmine_api_db::{create_batch_of_starkscan_events, get_last_timestamp_for_protocol_event};
-use carmine_api_starknet::starkscan::{fetch_events, update_lending_protocol_events};
+use carmine_api_core::network::Protocol;
+use carmine_api_starknet::starkscan::update_lending_protocol_events;
 use dotenvy::dotenv;
 
 #[tokio::main]
