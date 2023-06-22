56a2b51b7e48563db7d4d1edb2aed27318ba4b20
DaveVodrazka <dave.vodrazka@gmail.com>
2023-06-22 12:47:56 +0200

    feat: fetch block range

--- carmine-api-starknet/src/bin/fetch.rs
+++ carmine-api-starknet/src/bin/fetch.rs
@@ -1,7 +1,7 @@
 use std::env;
 
 use carmine_api_core::network::Protocol;
-use carmine_api_starknet::starkscan::update_lending_protocol_events;
+use carmine_api_starknet::starkscan::update_block_range;
 use dotenvy::dotenv;
 
 #[tokio::main]
@@ -10,6 +10,5 @@ async fn main() {
     env::set_var("ENVIRONMENT", "docker");
     env::set_var("DB_IP", "34.76.28.66");
 
-    update_lending_protocol_events(&Protocol::Hashstack).await;
-    update_lending_protocol_events(&Protocol::ZkLend).await;
+    update_block_range(&Protocol::ZkLend, 48600, 48700).await;
 }
