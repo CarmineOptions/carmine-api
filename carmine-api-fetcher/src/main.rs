f54993fce29be0850f1f86a18118b9a7ce98ad04
DaveVodrazka <dave.vodrazka@gmail.com>
2023-08-30 23:30:36 +0200

    fix: add endpoint for GCP

--- carmine-api-fetcher/src/main.rs
+++ carmine-api-fetcher/src/main.rs
@@ -1,3 +1,6 @@
+use std::env;
+
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 use tokio::time::{sleep, Duration};
 
 use carmine_api_core::telegram_bot;
@@ -5,27 +8,52 @@ use carmine_api_starknet::{update_database_amm_state, update_database_events};
 
 const UPDATE_DELAY: u64 = 20;
 
-#[actix_web::main]
-async fn main() {
-    loop {
-        if let Err(err) = actix_web::rt::spawn(async { update_database_events().await }).await {
-            // failed, probably network overload, wait to send message
-            sleep(Duration::from_secs(10)).await;
-            println!("update_database_events panicked\n{:?}", err);
-            telegram_bot::send_message("Carmine API `update_database_events` just panicked").await;
-        } else {
-            println!("Database updated with events");
-        }
-        sleep(Duration::from_secs(UPDATE_DELAY)).await;
-        if let Err(err) = actix_web::rt::spawn(async { update_database_amm_state().await }).await {
-            // failed, probably network overload, wait to send message
-            sleep(Duration::from_secs(10)).await;
-            println!("Update database amm state panicked\n{:?}", err);
-            telegram_bot::send_message("Carmine API `update_database_amm_state` just panicked")
-                .await;
-        } else {
-            println!("Database updated with AMM state");
-        }
-        sleep(Duration::from_secs(UPDATE_DELAY)).await;
+const LOCAL_IP: &str = "127.0.0.1";
+const DOCKER_IP: &str = "0.0.0.0";
+
+fn ip_address() -> &'static str {
+    match env::var("ENVIRONMENT") {
+        Ok(v) if v == "local" => LOCAL_IP,
+        _ => DOCKER_IP,
     }
+}
+
+#[get("/")]
+async fn liveness() -> impl Responder {
+    HttpResponse::Ok().body("alive")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    actix_web::rt::spawn(async move {
+        loop {
+            if let Err(err) = actix_web::rt::spawn(async { update_database_events().await }).await {
+                // failed, probably network overload, wait to send message
+                sleep(Duration::from_secs(10)).await;
+                println!("update_database_events panicked\n{:?}", err);
+                telegram_bot::send_message("Carmine API `update_database_events` just panicked")
+                    .await;
+            } else {
+                println!("Database updated with events");
+            }
+            sleep(Duration::from_secs(UPDATE_DELAY)).await;
+            if let Err(err) =
+                actix_web::rt::spawn(async { update_database_amm_state().await }).await
+            {
+                // failed, probably network overload, wait to send message
+                sleep(Duration::from_secs(10)).await;
+                println!("Update database amm state panicked\n{:?}", err);
+                telegram_bot::send_message("Carmine API `update_database_amm_state` just panicked")
+                    .await;
+            } else {
+                println!("Database updated with AMM state");
+            }
+            sleep(Duration::from_secs(UPDATE_DELAY)).await;
+        }
+    });
+
+    HttpServer::new(|| App::new().service(liveness))
+        .bind((ip_address(), 8080))?
+        .run()
+        .await
 }
