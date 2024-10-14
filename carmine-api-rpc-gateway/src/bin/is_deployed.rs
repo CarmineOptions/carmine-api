4f5f5970cedc3b1c7671882a28c60967a302a444
DaveVodrazka <dave.vodrazka@gmail.com>
2024-10-14 16:36:12 +0200

    fix: update rpc call

---
+++ carmine-api-rpc-gateway/src/bin/is_deployed.rs
@@ -0,0 +1,21 @@
+use carmine_api_rpc_gateway::{is_contract_deployed, RpcNode};
+use dotenvy::dotenv;
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+
+    let is_deployed_result = is_contract_deployed(
+        RpcNode::CarmineJunoNode,
+        "0x4a260577bccdc4912e5850b795986fddbeeb4e8e8db8f67ff0537fe8d231459".to_string(),
+        740000,
+    )
+    .await;
+
+    if let Ok(is_deployed) = is_deployed_result {
+        match is_deployed {
+            true => println!("is deployed"),
+            false => println!("is NOT deployed"),
+        }
+    }
+}
--- carmine-api-starknet/src/amm_state.rs
+++ carmine-api-starknet/src/amm_state.rs
@@ -1,7 +1,7 @@
 use std::time::{Duration, Instant};
 
 use carmine_api_core::{
-    network::Network,
+    network::{Network, NEW_AMM_GENESIS_BLOCK_NUMBER},
     types::{DbBlock, OracleName, TokenPair},
 };
 use carmine_api_db::{
@@ -132,8 +132,7 @@ impl AmmStateObserver {
             }
         };
 
-        // let start = NEW_AMM_GENESIS_BLOCK_NUMBER; // new AMM deployed
-        let start = 642000;
+        let start = NEW_AMM_GENESIS_BLOCK_NUMBER; // new AMM deployed
         let finish = i64::try_from(last_block_starknet.block_number).unwrap();
 
         let holes = get_pool_state_block_holes(start, finish, &Network::Mainnet);
